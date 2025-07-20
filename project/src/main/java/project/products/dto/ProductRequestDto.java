6a4874ae35aa942fb4cb39a63487fb2ffd2856d0
jongmine <jongmine.ch@gmail.com>
2025-07-20 18:13:20 +0900

    feat: DTO에 유효성 애너테이션 적용

---
+++ project/src/main/java/project/products/dto/ProductRequestDto.java
@@ -0,0 +1,35 @@
+package project.products.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import project.products.domain.Product;
+import project.products.validator.ForbiddenWords;
+
+@Getter
+public class ProductRequestDto {
+
+    @NotBlank(message = "상품 이름은 필수입니다.")
+    @Size(max = 15, message = "상품 이름은 최대 15자까지 가능합니다.")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9가-힣 ()\\[\\]+\\-&/_]*$",
+            message = "허용되지 않은 특수 문자가 포함되어 있습니다."
+    )
+    @ForbiddenWords(value = {"카카오"}, message = "금지된 단어가 포함되어 있습니다: 카카오")
+    private String name;
+
+    @Min(value = 0, message = "가격은 0 이상이어야 합니다.")
+    private int price;
+
+    private String imageUrl;
+
+    public Product toEntity() {
+        return Product.builder()
+                .name(name)
+                .price(price)
+                .imageUrl(imageUrl)
+                .build();
+    }
+}
