1e59a54a1d49cd161a6f3e79a5ed394bfc557abd
jongmine <jongmine.ch@gmail.com>
2025-07-25 04:19:45 +0900

    feat: wishlist 테이블 및 도메인 클래스 추가

--- project/src/main/resources/schema.sql
+++ project/src/main/resources/schema.sql
@@ -12,3 +12,12 @@ CREATE TABLE IF NOT EXISTS users (
     role VARCHAR(50) NOT NULL,
     refresh_token VARCHAR(512)
 );
+
+CREATE TABLE wishlist
+(
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    CONSTRAINT fk_wishlist_user FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_wishlist_product FOREIGN KEY (product_id) REFERENCES product (id)
+);
